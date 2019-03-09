91f25db5c23c6401f0375ba8c99ade55547f2bea
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-03-09 16:01:54 +0000

    Update extractlastname6kyu.js

--- extractlastname6kyu.js
+++ extractlastname6kyu.js
@@ -1,3 +1,10 @@
+Given a text, for example:
+
+const inputText = "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+
+get an array of last names of people named Michael. The result should be:
+["Jordan", "Johnson", "Green", "Wood"]
+
 
 const extract =(input)=>{
     const myMatch = input.match(/Michael\s*\w+/g).toString().match(/\w+/g)
