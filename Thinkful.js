555d5209730591dd1633984cc33a63329d78cabe
Sarpong Abasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-03-30 21:18:32 +0000

    Create Thinkful.js

---
+++ Thinkful.js
@@ -0,0 +1,12 @@
+/*
+Write a function named repeater() that takes two arguments 
+(a string and a number), and returns a new string where the input string is repeated that many times.
+
+eg.. 
+Repeater.repeat("a", 5)
+"aaaaa"
+*/
+
+const repeater = (str, num) => {
+    return str.repeat(num)
+}
