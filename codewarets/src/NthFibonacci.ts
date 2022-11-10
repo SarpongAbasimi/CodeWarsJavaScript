3c00e39acce2ed1723ea19395b22b09a024b8cb8
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2022-11-10 21:11:38 +0000

    Todo, added katas to work on

---
+++ codewarets/src/NthFibonacci.ts
@@ -0,0 +1,10 @@
+// I love Fibonacci numbers in general, 
+// but I must admit I love some more than others.
+// I would like for you to write me a 
+// function that when given a number (n) 
+// returns the n-th number in the Fibonacci Sequence.
+// For example:
+
+// To do
+// Kata - https://www.codewars.com/kata/522551eee9abb932420004a0/train/javascript
+
---
+++ codewarets/src/specialMultiples.ts
@@ -0,0 +1,1 @@
+// Kata - https://www.codewars.com/kata/55e785dfcb59864f200000d9
