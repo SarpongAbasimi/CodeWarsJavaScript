fdf328912c1f3a619f0e6dce6c4719f55bdad52b
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2022-11-15 21:32:12 +0000

    Search not working ☹️

--- codewarets/src/findNumber.ts
+++ codewarets/src/findNumber.ts
@@ -1,29 +1,30 @@
-const findNumber: (inputArray: number[], valueToSearch: number) => number 
-= (a,b) => {
-    if(a.length == 0){
-        return -1
-    }
+// const findNumber: (inputArray: number[], valueToSearch: number) => number 
+// = (a,b) => {
+//     if(a.length == 0){
+//         return -1
+//     }
 
-    if(a.length == 1){
-        return a[0] == b ? b : -1
-    } else {        
-        let fullLenghtOfArray = a.length
-        let midPointOfArray = (fullLenghtOfArray) / 2
+//     if(a.length == 1){
+//         return a[0] == b ? b : -1
+//     } else {      
+//         let max = a.length
+//         let midPointOfArray = (max) / 2
 
-        if(midPointOfArray > b){
-            a.splice(Math.round(midPointOfArray))
-            return findNumber(a, b)
-        } else {
-            a.splice(0, Math.round(midPointOfArray))
-            return findNumber(a, b)
-        }
-    }
-}
+//         if(midPointOfArray > b){
+//             a.splice(Math.round(midPointOfArray))
+//             return findNumber(a, b)
+//         } else {
+
+//             a.splice(0, Math.round(midPointOfArray))
+//             return findNumber(a, b)
+//         }
+//     }
+// }
 
 
-let res: number = findNumber([1,2,3,4,5,6,7,8,9,10], 4)
+// let res: number = findNumber([1,2,3,4,5,6,7,8,9,10], 7)
 
-console.log(res)
+// console.log(res)
 
 
 
