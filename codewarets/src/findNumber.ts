f0b8a793f9c327a094adf066b68cc9096ddac7d8
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2022-11-16 22:11:55 +0000

    Understanding search

--- codewarets/src/findNumber.ts
+++ codewarets/src/findNumber.ts
@@ -1,30 +1,38 @@
-// const findNumber: (inputArray: number[], valueToSearch: number) => number 
-// = (a,b) => {
-//     if(a.length == 0){
-//         return -1
-//     }
+// We have an array of data
+// devide the data into half
+// check if the data is greater than or less than the provided value
+// if greater than, get the other half and try again
 
-//     if(a.length == 1){
-//         return a[0] == b ? b : -1
-//     } else {      
-//         let max = a.length
-//         let midPointOfArray = (max) / 2
+const findNumber: (inputArray: number[], valueToSearch: number) => number 
+= (inputArrayValue, searchNumber) => {
 
-//         if(midPointOfArray > b){
-//             a.splice(Math.round(midPointOfArray))
-//             return findNumber(a, b)
-//         } else {
+    let max = inputArrayValue.length
+    let low =   0
 
-//             a.splice(0, Math.round(midPointOfArray))
-//             return findNumber(a, b)
-//         }
-//     }
-// }
+    function innerFunc(array: number[], serachValue:number, start: number, inputSize: number): number{
+        if(array.length == 0){
+            return - 1
+        } else {
+            let midPoint: number = Math.floor((start + inputSize)/ 2)
+    
+            if(array[midPoint] == serachValue){
+                return array[midPoint]
+            } else if (midPoint < serachValue){
+                low = midPoint + 1
+                return innerFunc(array, searchNumber, low, inputSize)
+            } else {
+                return innerFunc(array, searchNumber, low - 1, max)
+            }
+        }
+    }
+    
+    return innerFunc(inputArrayValue, searchNumber, low, max)
+}
 
 
-// let res: number = findNumber([1,2,3,4,5,6,7,8,9,10], 7)
+let res: number = findNumber([1,2,3,4,5,6,7,8,9,10], 4)
 
-// console.log(res)
+console.log(res)
 
 
 
