0017f9606e078371bb7f174e05e026f46cd8c8cd
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2022-12-19 21:32:37 +0000

    Added array input using recursion

---
+++ codewarets/src/sumAll.ts
@@ -0,0 +1,15 @@
+type InputArray = number[];
+
+const sumElementsInArray = (input: InputArray): number => {
+  const add = (processedArray: number[], accumulation: number): number => {
+    if (processedArray.length == 0) {
+      return accumulation;
+    } else {
+      let nn = processedArray.splice(0, 1);
+      return add(processedArray, accumulation + nn[0]);
+    }
+  };
+  return add(input, 0);
+};
+
+console.log(sumElementsInArray([1, 2, 3, 4, 5]));
