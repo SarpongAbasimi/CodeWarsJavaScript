8b50a3d2d47949e13bdfc587788804ebe705cf81
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2023-01-06 21:19:34 +0000

    short form is not working

--- codewareScala/src/main/scala/Main.scala
+++ codewareScala/src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import algos.PlusOne
 object Main {
   def main(args: Array[String]): Unit = {
-    val request = PlusOne(Array(3,4,8,19))
+    val request = PlusOne(Array(9,8,7,6,5,4,3,2,1,9))
     val result = request.plusOne
     println(result.toList)
   }
--- codewareScala/src/main/scala/algos/PlusOne.scala
+++ codewareScala/src/main/scala/algos/PlusOne.scala
@@ -1,23 +1,11 @@
 package algos
 
-//wip
 case class PlusOne (digits: Array[Int]){
   def plusOne: Array[Int] = {
     val digitSize = digits.length - 1
-
-    def compute(input: Array[Int], index: Int): Array[Int] ={
-      if(index < 0){
-        input
-      } else {
-        if(input(index) < 9){
-          compute(input, index - 1)
-        } else {
-          compute(input, index - 1)
-        }
-
-      }
+    def compute(input: Array[Int], index: Int): Array[Int] = {
+      ???
     }
-
     compute(digits, digitSize)
   }
 }
